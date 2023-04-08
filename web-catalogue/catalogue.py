06081abf4cc03f4113a9150db41155c710ed9aac
Max Heilingbrunner <maxheilingbrunner@xMacBook.fritz.box>
2023-04-08 19:44:58 +0200

    final optimized release

--- web-catalogue/catalogue.py
+++ web-catalogue/catalogue.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Setting the secret key used by Flask to securely sign cookies and other data.
 app.config['SECRET_KEY'] = 'DO_NOT_LOSE_THIS_KEY'
 
-@app.route("/catalogue")
+@app.route("/")
 def catalogue():
     # Rendering the catalogue
     return render_template("catalogue.html"), 200
